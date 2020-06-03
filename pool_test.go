45787d0fc89551a1a82751992bd791877b300368
Oleksiy Pyltsov <valar999@gmail.com>
2020-06-03 21:39:59 +0300

    go fmt

--- pool_test.go
+++ pool_test.go
@@ -16,17 +16,17 @@ func TestCache(t *testing.T) {
 
 func TestSimultaneous(t *testing.T) {
 	pool := NewPool()
-	for i := 0; i < 10; i++ {
-	go func() {
-		var n int32 = 0
-		for i := 0; i < 1000; i++ {
-			pool.getConnection(n)
-			n++
-			if n >= 9 {
-				n = 0
+	for i := 0; i < 1000; i++ {
+		go func() {
+			var n int32 = 0
+			for i := 0; i < 1000; i++ {
+				pool.getConnection(n)
+				n++
+				if n >= 9 {
+					n = 0
+				}
 			}
-		}
-	}()
+		}()
 	}
 	conn1 := pool.getConnection(1)
 	conn2 := pool.getConnection(1)
