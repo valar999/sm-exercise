c58f437be3371462cb2924a160893b4de9302162
Oleksiy Pyltsov <valar999@gmail.com>
2020-06-04 23:41:49 +0300

    Added comments

--- pool.go
+++ pool.go
@@ -32,32 +32,45 @@ func NewPool() Pool {
 }
 
 func (pool *pool) getConnection(addr int32) Connection {
+	// Lock all Pool
 	pool.Lock()
 	if pool.isShutdown {
 		pool.Unlock()
 		return nil
 	}
+	// get cache item
 	c, ok := pool.cache[addr]
 	if ok {
+		// if item exists, we can release global lock
 		pool.Unlock()
+		// we need the Lock here due to wait for connection appears 
+		// in pool.cache[addr].conn
 		c.Lock()
 		c.Unlock()
 	} else {
+		// no cache item, we acquire Pool lock, so we are alone here
+		// create new item with channel to receive connection from
+		// open() or from onNewRemoteConnection
 		c = &Conn{
 			ch: make(chan Connection, 2),
 		}
+		// store new item
 		pool.cache[addr] = c
 
+		// lock this item (addr) and release global lock
 		c.Lock()
 		pool.Unlock()
 
 		openConn := NewConn(addr, openDelay)
 		go func(ch chan Connection) {
+			// run open() in goroutine put connection to channel
 			openConn.open()
 			ch <- openConn
 		}(c.ch)
 		select {
 		case conn := <-c.ch:
+			// got a connection from open() or from onNew..
+			// lock Pool to store connection
 			pool.Lock()
 			pool.cache[addr].conn = conn
 			pool.Unlock()
@@ -75,11 +88,16 @@ func (pool *pool) onNewRemoteConnection(remotePeer int32, conn Connection) {
 	}
 	c, ok := pool.cache[remotePeer]
 	if ok {
+		// non-blocking send "conn" to channel
+		// doesn't matter success or not
+		// we need only one remote connection to accept
+		// if we are in the middle of opening connection
 		select {
 		case c.ch <- conn:
 		default:
 		}
 	} else {
+		// just store received connection, lock acquired so it's safe
 		pool.cache[remotePeer] = &Conn{conn: conn}
 	}
 }
@@ -87,10 +105,13 @@ func (pool *pool) onNewRemoteConnection(remotePeer int32, conn Connection) {
 func (pool *pool) shutdown() {
 	pool.Lock()
 	defer pool.Unlock()
+	// lock all items to wait open() or other procedures
 	for _, c := range pool.cache {
 		c.Lock()
+		// release locks on return
 		defer c.Unlock()
 	}
+	// close all connections
 	for _, c := range pool.cache {
 		c.conn.close()
 	}
